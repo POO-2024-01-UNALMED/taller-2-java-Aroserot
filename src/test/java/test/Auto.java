b672d7d20bf990bd224b35b9ff6cd36e6daf720e
Aroserot <aroserot@unal.edu.co>
2024-02-19 21:23:17 -0500

    Taller java

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,35 @@
+package test;
+
+public class Auto {
+	int registro;
+	int precio;
+	String modelo;
+	String marca;
+	Asiento [] asientos;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		int numeroAsientos = 0;
+		for (int i = 0; i < asientos.length; i++) {
+			
+			if (asientos[i] != null) {
+				numeroAsientos++;
+			}
+		}
+		return numeroAsientos;
+	}
+	
+	String verificarIntegridad() {
+		if (this.registro == this.motor.registro) {
+			for (int i = 0; i < asientos.length; i++) {
+				if (asientos[i] != null && asientos[i].registro != registro) {
+					return "Las piezas no son originales";
+				}
+			}
+			return "Auto original";
+		}
+		else {
+			return "Las piezas no son originales";
+		}
+	}
+}
